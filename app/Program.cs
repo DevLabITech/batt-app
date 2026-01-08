dd23cc6b27595b5f00a73ee1e2de9b4772c9175b
Albert Bici <albertbici.dev@gmail.com>
2026-01-08 19:41:28 +0100

    init structure

---
+++ app/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace app
+{
+    class Start
+    {
+        public static void Main(string[] args)
+        {
+            string[] lettereTabella = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
+            Console.Write($" "); // fix -> numeri
+            foreach (var column in lettereTabella)
+            {
+                Console.Write($"  {column}");
+            }
+            Console.WriteLine($"");
+            for (int i = 0; i < lettereTabella.Length; i++)
+            {
+                int count = 1;
+
+                Console.Write($"{i + 1}");
+                Console.Write(($"{i + 1}".Length % 2 == 1) ? " " : ""); // fix -> numeri
+                foreach (var column in lettereTabella)
+                {
+                    if (count == 1)
+                        Console.Write($" *");
+                    else
+                        Console.Write($"  *");
+                    count++;
+                }
+                Console.WriteLine($"");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
